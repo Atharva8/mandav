7d71484774d4a7ec59bb0e58ce87bec5004db49f
Atharva <ath284.ap@gmail.com>
2021-05-01 11:19:43 +0530

    Update settings.py

--- money/settings.py
+++ money/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
